e807bed2e0a26ed0310d5a8e09d019c103d5fb25
Greg <g.wastelain@gmail.com>
2019-09-24 12:32:02 +0200

    cursor update handler

--- assets/js/updateCursor.js
+++ assets/js/updateCursor.js
@@ -1,3 +1,26 @@
 (() => {
   //
+  let idCursor = document.getElementById('increase_priceCursor');
+  let countCursor = document.getElementById('count_cursor');
+  let buttonCursor = document.getElementById('purchase_cursor');
+
+  buttonCursor.addEventListener('click', () => {
+    let priceCursor = parseInt(idCursor.innerHTML);
+
+    if (priceCursor>compteur) {
+      console.log('pasassezdargent');
+    }
+    else {
+      idCursor.innerHTML = Math.floor(parseInt(idCursor.innerHTML)*1.15);
+      countCursor.innerHTML = parseInt(countCursor.innerHTML)+1;
+      compteur=compteur-priceCursor;
+      if (cursorNumber===0) {
+      buyCursor('number',1);
+      }
+      else {
+      buyCursor('timediv',2);
+      }
+      cursorNumber++;
+    }
+  });
 })();
