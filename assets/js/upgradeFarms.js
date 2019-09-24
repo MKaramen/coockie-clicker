597a529c4ead47b2099c0b508620a3c6ffda0e23
Philippe Gilles <philgdep@gmail.com>
2019-09-24 15:06:23 +0200

    fix cursor buy

--- assets/js/upgradeFarms.js
+++ assets/js/upgradeFarms.js
@@ -4,26 +4,26 @@ let farmsUpgradeStore = [
   {
     cost: 11000,
     condition: null,
-    name: "Forwards from farms"
+    name: 'Forwards from farms'
   },
   {
     cost: 55000,
     condition: null,
-    name: "Steel-plated rolling pins"
+    name: 'Steel-plated rolling pins'
   },
   {
     cost: 550000,
     condition: null,
-    name: "Lubricated dentures	"
+    name: 'Lubricated dentures	'
   }
 ];
 
 (() => {
-  document.getElementById("upgradeCursor").addEventListener("click", () => {
+  document.getElementById('upgradeCursor').addEventListener('click', () => {
     let upgrade = farmsUpgradeStore[farmsUpgradeLevel];
     if (compteur > upgrade.cost) {
       compteur = compteur - upgrade.cost;
-      buyFarms("multiply", 2);
+      buyFarms('multiply', 2);
       farmsUpgradeLevel++;
     }
     console.log(farmsLoopValue);
