d06795ae0c0a21298e773b52ae0948d27fc06399
MKaramen <matis.kara@gmail.com>
2019-09-24 14:17:11 +0200

    change class to id + buyCursor change timeDiv to number

--- assets/js/updateCursor.js
+++ assets/js/updateCursor.js
@@ -1,24 +1,22 @@
 (() => {
   //
-  let idCursor = document.getElementById('increase_priceCursor');
-  let countCursor = document.getElementById('count_cursor');
-  let buttonCursor = document.getElementById('purchase_cursor');
+  let idCursor = document.getElementById("increase_priceCursor");
+  let countCursor = document.getElementById("count_cursor");
+  let buttonCursor = document.getElementById("purchase_cursor");
 
-  buttonCursor.addEventListener('click', () => {
+  buttonCursor.addEventListener("click", () => {
     let priceCursor = parseInt(idCursor.innerHTML);
 
-    if (priceCursor>compteur) {
-      console.log('pasassezdargent');
-    }
-    else {
-      idCursor.innerHTML = Math.floor(parseInt(idCursor.innerHTML)*1.15);
-      countCursor.innerHTML = parseInt(countCursor.innerHTML)+1;
-      compteur=compteur-priceCursor;
-      if (cursorNumber===0) {
-      buyCursor('number',1);
-      }
-      else {
-      buyCursor('timediv',2);
+    if (priceCursor > compteur) {
+      console.log("pasassezdargent");
+    } else {
+      idCursor.innerHTML = Math.floor(parseInt(idCursor.innerHTML) * 1.15);
+      countCursor.innerHTML = parseInt(countCursor.innerHTML) + 1;
+      compteur = compteur - priceCursor;
+      if (cursorNumber === 0) {
+        buyCursor("number", 1);
+      } else {
+        buyCursor("number", 1);
       }
       cursorNumber++;
     }
