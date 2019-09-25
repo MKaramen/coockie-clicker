55758d51fd28978c2ba29bed6f4c1e313394a089
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2019-09-25 10:22:35 +0200

    setSaveValues

--- assets/js/setSavedValues.js
+++ assets/js/setSavedValues.js
@@ -2,8 +2,12 @@ const setSavedValues = () => {
   Object.keys(saveObject.purchase).forEach(building => {
     console.log(saveObject.purchase[building]);
     let setUpdateLevel = saveObject.purchase[building].updateLevel;
+    let setUpdatePrice = saveObject.purchase[building].updatePrice;
     console.log("count_" + building);
     document.getElementById("count_" + building).innerText = setUpdateLevel;
+    document.getElementById(
+      "increase_price" + building
+    ).innerText = setUpdatePrice;
   });
 };
 
--- assets/js/updateCursor.js
+++ assets/js/updateCursor.js
@@ -1,22 +1,24 @@
 (() => {
   //
-  let idCursor = document.getElementById('increase_priceCursor');
-  let countCursor = document.getElementById('count_cursor');
-  let buttonCursor = document.getElementById('purchase_cursor');
+  let idCursor = document.getElementById("increase_priceCursor");
+  let countCursor = document.getElementById("count_cursor");
+  let buttonCursor = document.getElementById("purchase_cursor");
 
-  buttonCursor.addEventListener('click', () => {
+  buttonCursor.addEventListener("click", () => {
     let priceCursor = parseInt(idCursor.innerHTML);
 
     if (priceCursor > saveObject.compteur) {
       console.log("T'as pas assez d'argent !");
     } else {
-      idCursor.innerHTML = Math.round(parseInt(idCursor.innerHTML) * 1.15);
+      let price = Math.round(parseInt(idCursor.innerHTML) * 1.15);
+      idCursor.innerText = price;
+      saveObject.purchase.cursor.updatePrice = price;
       countCursor.innerHTML = parseInt(countCursor.innerHTML) + 1;
       saveObject.compteur = saveObject.compteur - priceCursor;
       if (saveObject.purchase.cursor.updateLevel === 0) {
-        buyUpdate('cursor', 'number', 1);
+        buyUpdate("cursor", "number", 1);
       } else {
-        buyUpdate('cursor', 'number', 1);
+        buyUpdate("cursor", "number", 1);
       }
       saveObject.purchase.cursor.updateLevel++;
     }
